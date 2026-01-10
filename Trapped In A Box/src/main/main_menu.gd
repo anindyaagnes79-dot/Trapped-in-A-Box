b03684e853afe9e7bfa0f30708c9c55e3a3fe7e0
daffa.doj <daffa.doj@gmail.com>
2026-01-10 20:43:38 +0700

    feat: add credit panel

--- Trapped In A Box/src/main/main_menu.gd
+++ Trapped In A Box/src/main/main_menu.gd
@@ -5,3 +5,6 @@ func start(): get_tree().change_scene_to_file("res://level_select.tscn")
 func option(): $OptionPanel.visible = true
 	
 func quit(): get_tree().quit(0)
+
+
+func credit() -> void: $CreditPanel.visible = true
